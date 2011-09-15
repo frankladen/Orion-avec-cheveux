d17cd99aa5a45d72ca98553291693c49fbe9fcda
TyMarc <marc_antoine_hinse@hotmail.com>
2011-09-15 17:54:39 -0400

    Ajout d'un choix pour refresh les messages
    Signed-off-by: TyMarc <marc_antoine_hinse@hotmail.com>

--- src/TestChatPyro/client.py
+++ src/TestChatPyro/client.py
@@ -9,13 +9,20 @@ try:
     player1=player.Player()
     numSocket=myServer.getNumSocket(player1)
     print(numSocket)
+    print('(Q)uitter')
+    print('(R)efresh')
+    print('Appuyez sur Enter pour envoyer')
     print(myServer.getNewMessage(numSocket))
     while 1:
         n=input("Msg >> ")
-        if n.lower().rfind("q",0,1) != -1:
+        print(len(n))
+        if n.lower().rfind("q",0,1) != -1 and len(n) == 2:
             break
-        myServer.addMessage(n, numSocket)
-        print(myServer.getNewMessage(numSocket))
+        elif n.lower().rfind("r",0,1) != -1 and len(n) == 2:
+            print(myServer.getNewMessage(numSocket))
+        else:
+            myServer.addMessage(n.rstrip('\r'), numSocket)
+            print(myServer.getNewMessage(numSocket))
 except:
     print('Le serveur n\'existe pas.  Fermeture du programme')
 
