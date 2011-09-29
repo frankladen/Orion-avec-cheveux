f1cf1ac987b859582a4fcac8ccdbbbfa8b4ac783
Marc-Antoine <marc_antoine_hinse@hotmail.com>
2011-09-29 16:39:20 -0400

    Remove player est supposé marcher
    Signed-off-by: Marc-Antoine <marc_antoine_hinse@hotmail.com>

--- src/Emile/Client.py
+++ src/Emile/Client.py
@@ -145,6 +145,7 @@ class Controller():
         if self.view.currentFrame == self.view.gameFrame:
             self.sendMessage('a quitté la partie')
             self.server.removePlayer(self.playerIp, self.players[self.playerId].name, self.playerId)
+            self.players[self.playerId].units = []
         self.view.root.destroy()
         
     def startGame(self):
