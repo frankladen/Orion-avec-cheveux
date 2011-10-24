c7ed967b5e1bbffc98796770c7dda4bc030d81e0
frankladen <francois.allard.2@gmail.com>
2011-10-24 09:59:07 -0400

    ajout du module de constante
    Signed-off-by: frankladen <francois.allard.2@gmail.com>

---
+++ src/Francois/Constants.py
@@ -0,0 +1,25 @@
+class MenuType():
+    MAIN=1
+    WAITING_FOR_RALLY_POINT=2
+    MOTHERSHIP_BUILD_MENU=4
+
+class UnitType():
+    SCOUT=1
+    SPACE_ATTACK_UNIT=2
+
+class FlagState():
+    STANDBY=1
+    MOVE=2
+    PATROL=4
+    ATTACK=8
+    GATHER=16
+    BUILD=32
+    REPAIR=64
+    RESEARCH=128
+    DESTROY=256 
+
+class MoveSpeed():
+    SCOUT = 4.0
+    SPACE_ATTACK_UNIT = 2.0
+    MOTHERSHIP = 0.0
+   
