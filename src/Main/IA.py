1a6373ccbd4337ec6e089a37ea2934f5fb44f7ac
Karim <karimForza@gmal.com>
2011-12-13 16:32:26 -0500

    Build units par IA
    Signed-off-by: Karim <karimForza@gmal.com>

--- src/Main/IA.py
+++ src/Main/IA.py
@@ -6,12 +6,15 @@ from Player import *
 import random
 import socket
 import time
+from Building import*
 
 class IA(Player):
     def __init__(self, name, game, id , colorId):
         Player.__init__(self, name, game, id , colorId)       
         self.frameAction = 60
-        self.frameActuel = 0        
+        self.frameActuel = 0    
+        self.priority = (4, 1, 1, 3, 11)
+        self.maxUnits =(5,1,1,5,0,1)		
         
     def requeteModele(self): #methode que controleur va appeler
         if self.frameActuel == self.frameAction:
@@ -32,11 +35,21 @@ class IA(Player):
     def trouverRessource(self):
         for i in self.game.galaxy.solarSystemList:
             for j in i.nebulas:
-                if(self.game.players[self.id].inViewRange(j.position)):
-                    return j
+                if self.game.players[self.id].inViewRange(j.position) and j.gazQte > 0:
+                    temp = True
+                    for u in self.units:
+                        if u.flag.finalTarget == j:
+                            temp = False
+                    if temp:
+                        return j
             for j in i.asteroids:
-                if(self.game.players[self.id].inViewRange(j.position)):
-                    return j
+                if self.game.players[self.id].inViewRange(j.position) and j.mineralQte > 0:
+                    temp = True
+                    for u in self.units:
+                        if u.flag.finalTarget == j:
+                            temp = False
+                    if temp:
+                        return j
         return None
                     
     def envoyerCargo(self,ressource):
@@ -57,8 +70,7 @@ class IA(Player):
                 i.changeFlag(Target([i.position[0]+x,i.position[1]+y,0]), FlagState.MOVE)
                  
     def choixAction(self):
-        r = random.randint(1,2)
-    
+        r = random.randint(1,3)    
         if r == 1:
             self.explore()
         elif r == 2:
@@ -66,10 +78,53 @@ class IA(Player):
             if ressource != None:
                self.envoyerCargo(ressource)
         elif r == 3:
-            print("buildddd")
+            self.decisionBuildUnit()
         else:
             print("attaqueeeee")
+          
+    def decisionBuildUnit(self):
+        for i in self.priority:
+            if self.needBuild(i):
+                if self.canAfford(Unit.BUILD_COST[i][0],Unit.BUILD_COST[i][1], Unit.BUILD_COST[i][2]):
+                    b =self.getStandByBuilding(i)
+                    if b != None:
+                        b.addUnitToQueue(i)
+    
+    def haveBuilding(self, unitType):
+        for i in self.buildings:
+            if i.type == unitType:
+                return True
+        return False
+    
+    def needBuild(self, unitType):
+        if self.nbrUnit(unitType) < self.maxUnits[self.priority.index(unitType)]:
+            if unitType == Unit.TRANSPORT:
+                if self.haveBuilding(Building.UTILITY):
+                    return True
+            if unitType == Unit.SCOUT or Unit.CARGO:
+                return True #meme pour scout et les autres
+        return False
             
+    def getStandByBuilding(self, unitType):
+        if unitType == 4 or unitType == 1:
+            for i in self.motherships:
+                if i.flag.flagState == FlagState.STANDBY:
+                    return i 
+        elif unitType == 3: #or repaire
+            for i in self.buildings:
+                if i.type == Building.UTILITY:
+                    if i.flag.flagState  == FlagState.STANDBY:
+                        return i 
+            return None
+    
+    def nbrUnit(self,unitType):
+        nbr = 0
+        for i in self.units:
+            if i.type == unitType:
+                nbr = nbr+1  
+        return nbr          
+                
+              
 
 # Joueur IA 1 (stupid)       
 class IA1(IA): # hérite de la classe IA
